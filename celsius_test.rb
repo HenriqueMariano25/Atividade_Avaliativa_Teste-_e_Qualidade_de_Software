71f8f81e16f5d9ce851b1e43b213188a988ab6bd
HenriqueMariano25 <you@example.com>
2019-10-25 20:11:12 -0300

    Incrementa Test da Atividade 1

--- celsius_test.rb
+++ celsius_test.rb
@@ -9,4 +9,16 @@ class CelsiusTeste < MiniTest::Test
   def test_conversao_para_kelvin
     assert_equal 300.15, Celsius.new.converter(27.0, 'K')
   end
+
+  def test_opcao_invalida
+    assert_equal "Opção invalida", Celsius.new.converter(33, 'A')
+  end
+
+  def test_conversao_para_fahrenheit_uma_temperatura_negativa
+    assert_equal -25.6, Celsius.new.converter(-32.0, 'F')
+  end
+
+  def test_conversao_para_kelvin_uma_temperatura_negativa
+    assert_equal 256.15, Celsius.new.converter(-17.0, 'K')
+  end
 end
